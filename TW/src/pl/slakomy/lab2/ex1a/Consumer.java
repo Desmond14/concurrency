683503af537f9c587e08d43b38d24f2e7073ae27
Desmond14 <s.lakomy@gmail.com>
2013-10-20 17:00:57 +0200

    package name change

---
+++ TW/src/pl/slakomy/lab2/ex1a/Consumer.java
@@ -0,0 +1,36 @@
+package pl.slakomy.lab2.ex1a;
+
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class Consumer implements Runnable {
+
+	Semaphore full;
+	Semaphore empty;
+	Random rand = new Random();
+	
+	public Consumer(Semaphore full, Semaphore empty) {
+		this.full = full;
+		this.empty = empty;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(!Thread.interrupted())
+				consume();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.out.println("Consumer has been interrupted!");
+		}
+	}
+	
+	public void consume() throws InterruptedException {
+		full.acquire();
+		//taking product from a container goes here
+		empty.release();
+		TimeUnit.MILLISECONDS.sleep((rand.nextInt(1000)));	//time required to consume
+	}
+
+}
